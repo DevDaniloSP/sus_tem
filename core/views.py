c01d00c53eb835b01c4dd4b1ce141948d3f94aba
DevDaniloSP <107083647+DevDaniloSP@users.noreply.github.com>
2022-09-18 02:01:21 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from core.models import Medicamento
+from django.views import View
+import json
 
-
-
+class MedicamentoView(View):
+    def get(self, request):
+        data = list(Medicamento.objects.values())
+        formatted_data = json.dumps(data, ensure_ascii=False)
+        return HttpResponse(formatted_data, content_type="application/json")
